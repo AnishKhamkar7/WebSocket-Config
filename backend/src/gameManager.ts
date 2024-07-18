285b61d49bcdf367c68143e68ef7248ed5892e80
AnishKhamkar7 <anishkhamkar7@gmail.com>
2024-07-18 13:11:08 +0530

    Added request handler ro handle the user  messages

--- backend/src/gameManager.ts
+++ backend/src/gameManager.ts
@@ -1,21 +1,36 @@
 import { WebSocket } from "ws";
+import { INIT_GAME } from "./messages";
 
 
 export class GameManager{
     private games: Game[];
+    private pendingUser: WebSocket;
+    private users: WebSocket[];
     constructor(){
         this.games = []
     }
 
-    addUser(socket: WebSocket){
+    addUser(socket: WebSocket){ 
+        this.users.push(socket)
+        this.addHandler(socket)
 
     }
 
     removeUser(socket: WebSocket){
+        this.users.filter(user => user !== socket);
+        //stop the game here because the user left
 
     }
 
-    private handleMessage(){
-
+    private addHandler(socket: WebSocket){
+        socket.on('message',(data)=>{
+            //you can add grpc in this ection 
+            const message = JSON.parse(data.toString());
+            if(message.type == INIT_GAME){
+                if(this.pendingUser){
+                    //start the game
+                }
+            }
+        })
     }
 }
