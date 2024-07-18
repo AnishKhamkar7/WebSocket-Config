91c839f11084a508bba5c7094b8617ba0a568400
AnishKhamkar7 <anishkhamkar7@gmail.com>
2024-07-18 13:52:48 +0530

    Controller for intialization of the game when the players are matched

--- backend/src/gameManager.ts
+++ backend/src/gameManager.ts
@@ -1,13 +1,16 @@
 import { WebSocket } from "ws";
-import { INIT_GAME } from "./messages";
+import { INIT_GAME, MOVE } from "./messages";
+import { Game } from "./Game";
 
 
 export class GameManager{
     private games: Game[];
-    private pendingUser: WebSocket;
+    private pendingUser: WebSocket |  null;
     private users: WebSocket[];
     constructor(){
-        this.games = []
+        this.games = [];
+        this.pendingUser = null;
+        this.users = [];
     }
 
     addUser(socket: WebSocket){ 
@@ -26,9 +29,22 @@ export class GameManager{
         socket.on('message',(data)=>{
             //you can add grpc in this ection 
             const message = JSON.parse(data.toString());
-            if(message.type == INIT_GAME){
+            if(message.type === INIT_GAME){
                 if(this.pendingUser){
                     //start the game
+                    const game = new Game(this.pendingUser,socket)
+
+                    this.games.push(game);
+                    this.pendingUser = null;
+                }
+                else {
+                    this.pendingUser = socket;
+                }
+            }
+            if(message.type === MOVE){
+                const game = this.games.find(game => game.player1 === socket || game.player2 === socket);
+                if(game){
+                    game.makeMove(socket, message.move);
                 }
             }
         })
